38e2f8ca2f72cff3fac6e094343926446a075a82
Paul Drummond <paul.drummond@iode.co.uk>
2016-04-15 12:12:47 +0100

    User management section done

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,13 +1,28 @@
 'use strict';
 
-// Add any common hooks you want to share across services in here.
-// 
-// Below is an example of how a hook is written and exported. Please
-// see http://docs.feathersjs.com/hooks/readme.html for more details
-// on hooks.
+const globalHooks = require('../../../hooks');
+const auth = require('feathers-authentication').hooks;
 
-exports.myHook = function(options) {
-  return function(hook) {
-    console.log('My custom global hook ran. Feathers is awesome!');
-  };
+exports.before = {
+  all: [
+    auth.verifyToken(),
+    auth.populateUser(),
+    auth.restrictToAuthenticated()
+  ],
+  find: [],
+  get: [],
+  create: [],
+  update: [],
+  patch: [],
+  remove: []
+};
+
+exports.after = {
+  all: [],
+  find: [],
+  get: [],
+  create: [],
+  update: [],
+  patch: [],
+  remove: []
 };
