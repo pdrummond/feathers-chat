619f331b59e7923e477f504d7cc40f16d843b5d4
Paul Drummond <paul.drummond@iode.co.uk>
2016-04-15 12:17:58 +0100

    Fixed mistake where I changed the wrong hook file.

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,28 +1,13 @@
 'use strict';
 
-const globalHooks = require('../../../hooks');
-const auth = require('feathers-authentication').hooks;
+// Add any common hooks you want to share across services in here.
+//
+// Below is an example of how a hook is written and exported. Please
+// see http://docs.feathersjs.com/hooks/readme.html for more details
+// on hooks.
 
-exports.before = {
-  all: [
-    auth.verifyToken(),
-    auth.populateUser(),
-    auth.restrictToAuthenticated()
-  ],
-  find: [],
-  get: [],
-  create: [],
-  update: [],
-  patch: [],
-  remove: []
-};
-
-exports.after = {
-  all: [],
-  find: [],
-  get: [],
-  create: [],
-  update: [],
-  patch: [],
-  remove: []
+exports.myHook = function(options) {
+  return function(hook) {
+    console.log('My custom global hook ran. Feathers is awesome!');
+  };
 };
